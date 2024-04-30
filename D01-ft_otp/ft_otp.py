f720255476bd61b1f1d5d362ab89078760e3b8ca
catal mathis <42ecolepro@gmail.com>
2024-04-30 17:38:20 +0200

    jfhksjd

--- D01-ft_otp/ft_otp.py
+++ D01-ft_otp/ft_otp.py
@@ -1,4 +1,5 @@
 import argparse
+import hashlib
 
 def main():
     parser = argparse.ArgumentParser()
@@ -7,8 +8,17 @@ def main():
 
     args = parser.parse_args()
     if (args.g):
-        print(args.g)
+        with open("ft_otp.key", "w") as key:
+            with open(args.g, "r") as file:
+                str = file.read()
+                if (len(str) < 64):
+                    return print("File must be at least 64 characters long.")
+                key.write(str)
+                print("Key was successfully saved in ft_otp.key.")
+
     if (args.k):
-        print(args.k)
+        with open("ft_otp.key", "w") as key:
+            hash = hashlib.sha256(args.k.encode())
+            print(hash)
 
 main()
