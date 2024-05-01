a6b5a54ff2b0fbc4a2c2c1caf490d92a6282cbee
catal mathis <42ecolepro@gmail.com>
2024-05-01 18:10:42 +0200

    jfhksjd

--- D01-ft_otp/ft_otp.py
+++ D01-ft_otp/ft_otp.py
@@ -1,5 +1,24 @@
 import argparse
 import hashlib
+import time
+
+import hmac
+import hashlib
+
+def generate_totp(key, time, nb_char):
+    k = bytes.fromhex(key)
+    hash = hmac.new(k, time, hashlib.sha1).digest()
+    offset = hash[-1] & 0xf # 0xf = 00001111
+    # 0x7f = 01111111
+    # 0xff = 11111111
+    binary = ((hash[offset] & 0x7f) << 24) | \
+             ((hash[offset + 1] & 0xff) << 16) | \
+             ((hash[offset + 2] & 0xff) << 8) | \
+             (hash[offset + 3] & 0xff)
+
+    result = str(binary % 10 ** nb_char)
+    return result
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -10,15 +29,18 @@ def main():
     if (args.g):
         with open("ft_otp.key", "w") as key:
             with open(args.g, "r") as file:
-                str = file.read()
-                if (len(str) < 64):
+                k = file.read().split(" ")[0].split("\n")[0]
+                print(k)
+                if (len(k) < 64):
                     return print("File must be at least 64 characters long.")
-                key.write(str)
+                key.write(k)
                 print("Key was successfully saved in ft_otp.key.")
 
     if (args.k):
-        with open("ft_otp.key", "w") as key:
-            hash = hashlib.sha256(args.k.encode())
-            print(hash)
+        with open("ft_otp.key", "r") as key:
+            t = int(time.time() // 30)
+            t_binary = t.to_bytes(8, byteorder='big')
+            k = key.read()
+            print(generate_totp(k, t_binary, 6))
 
 main()
